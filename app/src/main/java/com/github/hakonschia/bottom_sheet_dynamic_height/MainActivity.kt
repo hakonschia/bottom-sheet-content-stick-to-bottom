1865cf7875bf7532f3392b4daa6a3410c836713b
Håkon Schia <hakonschia5@gmail.com>
2022-11-12 10:18:08 +0100

    Add toggle to switch between the two bottom sheets

--- app/src/main/java/com/github/hakonschia/bottom_sheet_dynamic_height/MainActivity.kt
+++ app/src/main/java/com/github/hakonschia/bottom_sheet_dynamic_height/MainActivity.kt
@@ -3,10 +3,7 @@ package com.github.hakonschia.bottom_sheet_dynamic_height
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -19,45 +16,91 @@ import com.github.hakonschia.bottom_sheet_dynamic_height.ui.theme.Bottomsheetdyn
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BottomsheetdynamicheightTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    val state = rememberModalBottomSheetState(ModalBottomSheetValue.Expanded)
+                var stickToBottom by remember { mutableStateOf(false) }
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Switch(checked = stickToBottom, onCheckedChange = { stickToBottom = !stickToBottom })
 
-                    ModalBottomSheetLayout2(
-                        sheetState = state,
-                        sheetBackgroundColor = Color.Red,
-                        sheetContent = {
-                            var maxHeight by remember { mutableStateOf(true) }
-
-                            Box(
-                                contentAlignment = Alignment.Center,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(if (maxHeight) 500.dp else 50.dp)
-                            ) {
-                                Button(onClick = { maxHeight = !maxHeight }) {
-                                    Text(text = "Expand")
-                                }
-                            }
-                        }
-                    ) {
-                        val scope = rememberCoroutineScope()
-                        Button(
-                            onClick = {
-                                scope.launch {
-                                    state.show()
-                                }
-                            }
-                        ) {
-                            Text(text = "Show")
-                        }
+                    if (stickToBottom) {
+                        StickToBottomBottomSheet()
+                    } else {
+                        DefaultBottomSheet()
                     }
                 }
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun DefaultBottomSheet() {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+        val state = androidx.compose.material.rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Expanded)
+
+        ModalBottomSheetLayout(
+            sheetState = state,
+            sheetBackgroundColor = Color.Red,
+            sheetContent = {
+                SheetContent()
+            }
+        ) {
+            val scope = rememberCoroutineScope()
+            Button(
+                onClick = {
+                    scope.launch {
+                        state.show()
+                    }
+                }
+            ) {
+                Text(text = "Show default bottom sheet")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun StickToBottomBottomSheet() {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+        val state = rememberModalBottomSheetState(ModalBottomSheetValue.Expanded)
+
+        ModalBottomSheetLayout2(
+            sheetState = state,
+            sheetBackgroundColor = Color.Red,
+            sheetContent = {
+                SheetContent()
+            }
+        ) {
+            val scope = rememberCoroutineScope()
+            Button(
+                onClick = {
+                    scope.launch {
+                        state.show()
+                    }
+                }
+            ) {
+                Text(text = "Show stick to bottom bottom sheet")
+            }
+        }
+    }
+}
+
+@Composable
+private fun SheetContent() {
+    var maxHeight by remember { mutableStateOf(true) }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(if (maxHeight) 350.dp else 50.dp)
+    ) {
+        Button(onClick = { maxHeight = !maxHeight }) {
+            Text(text = "Expand")
         }
     }
 }
